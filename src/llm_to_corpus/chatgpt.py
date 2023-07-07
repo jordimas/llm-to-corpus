96b12f9627b04915d99c2faf2455e92161fdadf0
Jordi Mas <jmas@softcatala.org>
2023-07-07 13:20:33 +0200

    Support for gpt-4 models

--- src/llm_to_corpus/chatgpt.py
+++ src/llm_to_corpus/chatgpt.py
@@ -3,15 +3,16 @@ import time
 
 
 class ChatGPT:
-    def __init__(self, prompt):
+    def __init__(self, prompt, model_name):
         self.prompt = prompt
+        self.model_name = model_name
 
     def query_thread(self, text, translations, index):
         #    print(f"translate_thread. Entry: {text}")
         while True:
             try:
                 completion = openai.ChatCompletion.create(
-                    model="gpt-3.5-turbo",
+                    model=self.model_name,
                     temperature=0,
                     max_tokens=2000,
                     messages=[
--- src/llm_to_corpus/models.py
+++ src/llm_to_corpus/models.py
@@ -6,6 +6,7 @@ from .bloom import Bloom
 class Models:
     MODELS_DESCRIPTION = {
         "gpt-3.5-turbo": "OpenAI most capable GPT-3.5 model",
+        "gpt-4": "OpenAI GPT 4.0 model",
         "mt0-xxl-mt": "Bloom's 13B parameter model finetuned on xP3",
     }
 
@@ -16,13 +17,13 @@ class Models:
         return self.MODELS_DESCRIPTION
 
     def get_model(self, model_name, prompt):
-        if model_name == "gpt-3.5-turbo":
+        if model_name.startswith("gpt"):
             key = os.environ.get("OPENAI_API_KEY")
             if key is None or len(key) == 0:
                 raise RuntimeError(
                     "You should set OPENAI_API_KEY environment variable with you OpenAI key"
                 )
-            model = ChatGPT(prompt)
+            model = ChatGPT(prompt, model_name)
         elif model_name == "mt0-xxl-mt":
             model = Bloom(prompt)
         else:
